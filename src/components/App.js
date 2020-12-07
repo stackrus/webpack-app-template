0e74354f9f3b0a311ffc17c1b2b088f76970dddd
2019_rk6_63_akhmedovre <rusteeck@icloud.com>
2020-12-07 21:04:41 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import "../styles/App.css"
+
+class App extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Project init by WebPack</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
