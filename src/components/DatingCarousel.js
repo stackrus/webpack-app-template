74860a7188fa3181eba3a95a04f744fff89e72ce
2019_rk6_63_akhmedovre <rusteeck@icloud.com>
2020-12-14 23:45:56 +0700

    - динамическое добавление фоографий в Carousel через .map

--- src/components/DatingCarousel.js
+++ src/components/DatingCarousel.js
@@ -1,28 +1,36 @@
 import React, {Component} from "react";
 import {Container, Carousel} from "react-bootstrap";
-import logo1 from '../../public/img/1.jpg'
-import logo2 from '../../public/img/2.jpg'
-import logo3 from '../../public/img/3.jpg'
-import logo4 from '../../public/img/4.jpg'
-import logo5 from '../../public/img/5.jpg'
+import photo1 from '../../public/img/1.jpg'
+import photo2 from '../../public/img/2.jpg'
+// import logo3 from '../../public/img/3.jpg'
+import photo3 from '../../public/img/4.jpg'
+import photo4 from '../../public/img/5.jpg'
 // import {React} from 'react-router-dom'
-const mas = [logo1, logo2, logo3, logo4, logo5]
+const photoMas = [photo1, photo2, photo3, photo4]
 
 class DatingCarousel extends Component {
     constructor(props) {
         super();
+        this.state = {
+            activeIndex: 0
+        };
 
+        this.handleSelect = this.handleSelect.bind(this)
     }
 
+    handleSelect (selectedIndex, e){
+        this.setState({activeIndex: selectedIndex})
+        console.log(this.state.activeIndex);
+    };
+
     render() {
-        console.log(this.props.iconsMas)
         return (
             <>
                 <Container>
                     <h1>Выбирай свою жертву</h1>
-                    <Carousel>
-                    {mas.map(logo => (
-                        <Carousel.Item>
+                    <Carousel activeIndex={this.state.activeIndex} onSelect={this.handleSelect}>
+                    {photoMas.map((logo, index) => (
+                        <Carousel.Item key={index}>
                             <img
                                 className={"d-block w-100"}
                                 src={logo}
