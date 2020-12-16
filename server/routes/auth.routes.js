b5065b140b3d516c6a251e82a2d84177d7921da4
2019_rk6_63_akhmedovre <rusteeck@icloud.com>
2020-12-16 20:36:14 +0700

    - сделал регистрацию с валидацией форм и хешированием пароля (библиотеки express-validator и bcryptjs)

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,28 +2,45 @@ const {Router} = require('express')
 const User = require('../models/User')
 const router = Router()
 const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
-    try{
-        const {email, password} = req.body
-        const candidate = await User.findOne({email: email})
+router.post(
+    '/register',
+    [
+        check('email', 'Неккоректный email').isEmail(),
+        check('password', 'Минимальная длина пароля 6 символов')
+            .isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
 
-        if (candidate){
-            return res.status(400).json({message: "Такой email уже используется"})
+            if (!errors.isEmpty()){
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: "Неккоректные данные при регистрации"
+                })
+            }
+
+            const {email, password} = req.body
+            const candidate = await User.findOne({email: email})
+
+            if (candidate) {
+                return res.status(400).json({message: "Такой email уже используется"})
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12)
+            const user = new User({email: email, password: hashedPassword})
+
+            await user.save()
+
+            res.status(201).json({message: "Пользователь успешно создан"})
+        } catch (e) {
+            res.status(500).json({message: "Что-то пошло не так, попробуйте снова"})
         }
-
-        const hashedPassword = await bcrypt.hash(password, 12)
-        const user = new User({email: email, password: hashedPassword})
-
-        await user.save()
-
-        res.status(201).json({message: "Пользователь успешно создан"})
-    }
-    catch (e) {
-        res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" })
-    }
-})
+    })
 
 
 // /api/auth/login
