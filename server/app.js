c293d8a692c5d86f8306f1a4f5219f4e6a42c432
2019_rk6_63_akhmedovre <rusteeck@icloud.com>
2020-12-16 19:43:12 +0700

    - подключение к mongoDb - localhost нв 5000 порту

--- server/app.js
+++ server/app.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express()
 const PORT = config.get('port') || 5000
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
+async function start(){
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
+    }
+    catch (e) {
+        console.log(e.message);
+        process.exit(1)
+    }
+}
+
+start()
+
