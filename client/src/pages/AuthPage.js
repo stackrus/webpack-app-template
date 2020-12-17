ff89425200e141cf0d0874ebb600969c0b9d36e2
2019_rk6_63_akhmedovre <rusteeck@icloud.com>
2020-12-17 19:27:48 +0700

    - TODO: крафтим страницу авторизации - добавлены нестилизованные бомж-элементы формы авторизации с обработчиками ввода

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,22 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 
-const AuthPage = () => (
-    <div className={"container"}>
-        <div className={"row"} align={"center"}>
-            <div className={"col"}>
-                <h1>Welcum to z club buddy</h1>
-            </div>
-        </div>
-        <div className={"row"} align={"center"}>
-            <div className={"col"}>
-                <h1>Auth Page</h1>
+const AuthPage = () => {
+
+    const [form, setForm] = useState({
+        email: "",
+        password: ""
+    })
+
+    const changeHandler = event => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
+    return (
+        <div className={"container"}>
+            <div className={"row"} align={"center"}>
+                <div className={"col"}>
+                    <h1>Welcum to z club buddy</h1>
+                </div>
             </div>
 
-            <div className={"col"}>
-                <h1>Auth Page Yopta</h1>
+            <div className={"row"} align={"center"}>
+                <div className={"col"}>
+                    <input placeholder={"Введите e-mail"}
+                           id={"email"}
+                           type={"text"}
+                           name={"email"}
+                           onChange={changeHandler}
+                    />
+                </div>
             </div>
+
+            <div className={"row"} align={"center"}>
+                <div className={"col"}>
+                    <input placeholder={"Введите пароль"}
+                           id={"password"}
+                           type={"password"}
+                           name={"password"}
+                           onChange={changeHandler}
+                    />
+                </div>
+            </div>
+
+            <div className={"row"} align={"center"}>
+                <div className={"col"}>
+                    <button className={"btn"}>Войти</button>
+                </div>
+                <div className={"col"}>
+                    <button className={"btn"}>Зарегистрироваться</button>
+                </div>
+            </div>
+
         </div>
-    </div>
-);
+    )
+};
 
 export default AuthPage;
